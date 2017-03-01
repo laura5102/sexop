44db9e8258ec51131129fbb2b7b1faba73d1c35c
liulei <liulei@secoo.com>
2017-03-01 17:20:08 +0800

    添加样式文件

--- dnsmanage/app/get_file.py
+++ dnsmanage/app/get_file.py
@@ -3,7 +3,7 @@ import paramiko
 t = paramiko.Transport(('',22))
 t.connect(username="", password="")
 sftp = paramiko.SFTPClient.from_transport(t)
-remote_path = '/tmp/SecooMobile_program'
-local_path = '/tmp/'
+remote_path = '/usr/local/openresty/nginx/conf/nginx.conf'
+local_path = 'F:/71_nginx.conf'
 sftp.get(remote_path,local_path)
 t.close()
